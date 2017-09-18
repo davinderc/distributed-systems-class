65b7cc7f9d139d184f2dd996bd15866e2c507388
Davinder Singh Chandhok <d.chandhok@ufsc.br>
2017-09-18 17:29:57 -0300

    Finish class 18-09-17

--- RMIProduct2/ProductServant.java
+++ RMIProduct2/ProductServant.java
@@ -1,7 +1,7 @@
 import java.rmi.*;
 
 public class ProductServant implements Product { 
-	public ProductServant (String n) throws RemoteException {
+	public ProductServant () throws RemoteException {
 		name = "Communication sent without problems";
 	}
 
@@ -11,9 +11,6 @@ public class ProductServant implements Product {
 		leitor.pegueString(name);
 		
 		return leitor;
-		
-		System.out.println("Cliente invocou o metodo getDescription()");
-		return "Eu sou" + name +". Sou campeao.";
 	}
 	private String name;
 }
--- RMIProduct2/ProductServer.java
+++ RMIProduct2/ProductServer.java
@@ -7,8 +7,8 @@ public class ProductServer{
 		try{ 
                         LocateRegistry.createRegistry(Registry.REGISTRY_PORT);  
 			System.out.println("construindo implementacao de servidores");
-			Product p1 = new ProductServant ();
-			Product p2 = new ProductServant ();
+			Product p1 = new ProductServant();
+			Product p2 = new ProductServant();
                         Product stub1 = (Product) UnicastRemoteObject.exportObject(p1, 0);
                         Product stub2 = (Product) UnicastRemoteObject.exportObject(p2, 0);
 		     	System.out.println("criando no SN binding para servidores");
